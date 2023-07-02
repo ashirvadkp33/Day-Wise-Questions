530ca9b8755a96beaa4008ec1df4399751a2e562
Ashirvad kar pathak <59271649+ashirvadkp33@users.noreply.github.com>
2023-07-02 09:23:08 +0530

    Create SearchInRotatedSortedArray.cpp

---
+++ 2 JULY/SearchInRotatedSortedArray.cpp
@@ -0,0 +1,41 @@
+//https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+
+int search(vector<int>& nums, int target) {
+        int n=nums.size();
+        int low=0,high=n-1,ans=-1;
+
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+
+            if(nums[mid]==target)
+            {
+                ans=mid;
+                break;
+            }
+
+             if(nums[mid]>=nums[low])
+            {
+               if(target>=nums[low] && target<=nums[mid])
+               {
+                   high=mid-1;
+               }
+               else
+               low=mid+1;
+            }
+            else 
+            {
+                 if(target>=nums[mid] && target<=nums[high])
+               {
+                   low=mid+1;
+               }
+               else 
+                   high=mid-1;
+                
+            }
+           
+        }
+
+
+        return ans;
+    }
